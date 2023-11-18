6177afbbd00f0b1834377f668227e505f3045796
Boris <bhasfire@gmail.com>
2023-11-18 16:42:17 -0600

    3D Object Arrow Added; Translucent Polyline; Smoother Tracking

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZTVkMzhjYS1kMDc4LTRlMzYtOWNkNy05ZTY4OTZiMzNkYWIiLCJpZCI6MTc4NjIwLCJpYXQiOjE3MDAyNjA4MDJ9.exY7Ym_WUi_59ZWn18FKzw8Ue62LzQhJmdKHCd8JxUE';
 
 let viewer;  // Declare the viewer at the top scope
+let isAnimating = false; // Flag to control the animation
 
 document.addEventListener('DOMContentLoaded', () => {
     viewer = new Cesium.Viewer('cesiumContainer', {
@@ -11,77 +12,124 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add Google's Photorealistic 3D Tiles
     const tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
         url: 'https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCbWqOhzPZhDPVbRS_xd4p9KsHTZKbmju4',
-        showCreditsOnScreen: true,
+        showCreditsOnScreen: false,
     }));
 
     fetch('data/route.gpx')
         .then(response => response.text())
         .then(gpxText => {
-            const route = parseGPX(gpxText);
-            console.log(route);  // Log to check the route data
-            visualizeRoute(route);
+            const data = parseGPX(gpxText);
+            console.log(data); // Log to check the route data
+
+            // Call preloadTerrainData here
+            preloadTerrainData(data.coordinates, viewer.terrainProvider, () => {
+                console.log("Terrain data preloaded");
+                document.getElementById('startButton').disabled = false;
+            });
+
+            visualizeRoute(data);
         })
         .catch(error => {
             console.error('Error fetching or parsing GPX file:', error);
         });
+
+
+    // Event listener for the start button
+    document.getElementById('startButton').addEventListener('click', () => {
+        isAnimating = true;
+    });
+
+    // Event listener for the pause button
+    document.getElementById('pauseButton').addEventListener('click', () => {
+        isAnimating = false;
+    });
 });
 
-function visualizeRoute(route) {
-    const positions = route.map(point => Cesium.Cartesian3.fromDegrees(point.lon, point.lat, point.ele));
+function preloadTerrainData(coordinates, terrainProvider, callback) {
+    const samplePoints = coordinates.map(point => Cesium.Cartographic.fromDegrees(point.lon, point.lat));
+    Cesium.sampleTerrainMostDetailed(terrainProvider, samplePoints).then(callback);
+}
+
+function visualizeRoute(data) {
+    const positions = data.coordinates.map(point => 
+        Cesium.Cartesian3.fromDegrees(point.lon, point.lat, point.ele));
     const totalPoints = positions.length;
     let currentIndex = 0;
     const speed = 0.1;
 
-    // Create the route polyline
+    // Create the polyline
     viewer.entities.add({
         polyline: {
             positions: positions,
-            width: 5,
-            material: Cesium.Color.RED,
+            width: 10,
+            material: new Cesium.PolylineGlowMaterialProperty({
+                color: Cesium.Color.RED.withAlpha(0.6)
+            }),
             clampToGround: true
         }
     });
 
-    // Create an entity to represent the current position
+    // Create the arrow entity
     const routeEntity = viewer.entities.add({
         position: positions[0],
-        point: {
-            pixelSize: 10,
-            color: Cesium.Color.BLUE
+        model: {
+            uri: './data/Arrow_FBXImport_GLTFExport_01.gltf',
+            minimumPixelSize: 64,
+            scale: 0.1
+        },
+        label: {
+            text: data.name,
+            font: '14pt monospace',
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            outlineWidth: 2,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+            pixelOffset: new Cesium.Cartesian2(0, -9)
         }
     });
 
     const terrainProvider = viewer.terrainProvider;
 
     viewer.clock.onTick.addEventListener(() => {
-        currentIndex = (currentIndex + speed) % totalPoints;
+        if (!isAnimating) return;
+
+        currentIndex += speed;
+        if (currentIndex >= totalPoints) currentIndex = 0;
+
         const floorIndex = Math.floor(currentIndex);
-        if (floorIndex < totalPoints) {
-            const currentPosition = positions[floorIndex];
+        const nextIndex = (floorIndex + 1) % totalPoints;
+        const interpolationFactor = currentIndex - floorIndex;
 
-            if (currentPosition) {
-                // Sample the terrain height at the current position
-                Cesium.sampleTerrainMostDetailed(terrainProvider, [Cesium.Cartographic.fromCartesian(currentPosition)])
-                    .then((updatedPositions) => {
-                        const updatedPosition = Cesium.Cartesian3.fromRadians(
-                            updatedPositions[0].longitude,
-                            updatedPositions[0].latitude,
-                            updatedPositions[0].height
-                        );
-                        routeEntity.position = updatedPosition;
-                    });
+        const interpolatedPosition = Cesium.Cartesian3.lerp(positions[floorIndex], positions[nextIndex], interpolationFactor, new Cesium.Cartesian3());
 
-                // Update camera view
+        // Calculate the heading
+        const startCartographic = Cesium.Cartographic.fromCartesian(positions[floorIndex]);
+        const endCartographic = Cesium.Cartographic.fromCartesian(positions[nextIndex]);
+        let heading = Math.atan2(endCartographic.longitude - startCartographic.longitude, endCartographic.latitude - startCartographic.latitude);
+
+        const headingOffset = Math.PI / 2; // Adjust this value as needed to align the arrow correctly
+        heading += headingOffset;
+
+        // Sample the terrain height and adjust the entity's position and orientation
+        Cesium.sampleTerrainMostDetailed(terrainProvider, [Cesium.Cartographic.fromCartesian(interpolatedPosition)])
+            .then((updatedPositions) => {
+                const updatedPosition = Cesium.Cartesian3.fromRadians(
+                    updatedPositions[0].longitude,
+                    updatedPositions[0].latitude,
+                    updatedPositions[0].height + 0.5 // Slight height above ground to avoid clipping
+                );
+                routeEntity.position = updatedPosition;
+                routeEntity.orientation = Cesium.Transforms.headingPitchRollQuaternion(updatedPosition, new Cesium.HeadingPitchRoll(heading, 0, 0));
+
+                // Smooth camera movement
                 viewer.camera.lookAt(
-                    currentPosition, // The target position to look at
+                    updatedPosition, 
                     new Cesium.HeadingPitchRange(
-                        viewer.camera.heading,
-                        Cesium.Math.toRadians(-30),
-                        100
+                        Cesium.Math.toRadians(0), // Heading
+                        Cesium.Math.toRadians(-30), // Pitch
+                        100 // Range
                     )
                 );
-            }
-        }
+            });
     });
 }
 
--- scripts/gpxParser.js
+++ scripts/gpxParser.js
@@ -4,16 +4,17 @@ function parseGPX(gpxText) {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(gpxText, "application/xml");
 
+  const name = xmlDoc.getElementsByTagName("name")[0]?.textContent; // Extract route name
   const trkpts = xmlDoc.getElementsByTagName("trkpt");
   const route = [];
 
   for (let i = 0; i < trkpts.length; i++) {
-    const trkpt = trkpts[i];
-    const lat = parseFloat(trkpt.getAttribute("lat"));
-    const lon = parseFloat(trkpt.getAttribute("lon"));
-    const ele = parseFloat(trkpt.getElementsByTagName("ele")[0].textContent);
-    route.push({ lat, lon, ele });
+      const trkpt = trkpts[i];
+      const lat = parseFloat(trkpt.getAttribute("lat"));
+      const lon = parseFloat(trkpt.getAttribute("lon"));
+      const ele = parseFloat(trkpt.getElementsByTagName("ele")[0].textContent);
+      route.push({ lat, lon, ele });
   }
 
-  return route;
+  return { name, coordinates: route };
 }
