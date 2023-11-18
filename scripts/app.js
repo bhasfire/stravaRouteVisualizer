28fd8ae9edc8077e0ee520332d8cd60159bb31b1
Boris <bhasfire@gmail.com>
2023-11-18 12:58:38 -0600

    Added 3D terrain with photorealistic 3D tiles

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,15 @@ let viewer;  // Declare the viewer at the top scope
 
 document.addEventListener('DOMContentLoaded', () => {
     viewer = new Cesium.Viewer('cesiumContainer', {
-        // ...viewer settings...
+        terrainProvider: Cesium.createWorldTerrain(),
+
     });
+
+    // Add Google's Photorealistic 3D Tiles
+    const tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+        url: 'https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCbWqOhzPZhDPVbRS_xd4p9KsHTZKbmju4',
+        showCreditsOnScreen: true,
+    }));
 
     fetch('data/route.gpx')
         .then(response => response.text())
@@ -21,16 +28,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function visualizeRoute(route) {
     const positions = route.map(point => Cesium.Cartesian3.fromDegrees(point.lon, point.lat, point.ele));
-    const totalPoints = positions.length; // Ensure this is accessible within the function
+    const totalPoints = positions.length;
     let currentIndex = 0;
-    const speed = 0.1; // Adjust speed as needed
+    const speed = 0.1;
 
     // Create the route polyline
     viewer.entities.add({
         polyline: {
             positions: positions,
             width: 5,
-            material: Cesium.Color.RED
+            material: Cesium.Color.RED,
+            clampToGround: true
         }
     });
 
@@ -43,8 +51,7 @@ function visualizeRoute(route) {
         }
     });
 
-    const heightAboveTrack = 100; // Height above the route in meters
-    const pitchAngle = -30; // Adjust for a better view
+    const terrainProvider = viewer.terrainProvider;
 
     viewer.clock.onTick.addEventListener(() => {
         currentIndex = (currentIndex + speed) % totalPoints;
@@ -53,20 +60,29 @@ function visualizeRoute(route) {
             const currentPosition = positions[floorIndex];
 
             if (currentPosition) {
-                routeEntity.position = currentPosition;
+                // Sample the terrain height at the current position
+                Cesium.sampleTerrainMostDetailed(terrainProvider, [Cesium.Cartographic.fromCartesian(currentPosition)])
+                    .then((updatedPositions) => {
+                        const updatedPosition = Cesium.Cartesian3.fromRadians(
+                            updatedPositions[0].longitude,
+                            updatedPositions[0].latitude,
+                            updatedPositions[0].height
+                        );
+                        routeEntity.position = updatedPosition;
+                    });
 
-                // Using lookAt to position the camera
+                // Update camera view
                 viewer.camera.lookAt(
                     currentPosition, // The target position to look at
                     new Cesium.HeadingPitchRange(
-                        viewer.camera.heading, // Keep current heading or adjust as needed
-                        Cesium.Math.toRadians(pitchAngle), // Pitch angle
-                        heightAboveTrack // Height above the target position
+                        viewer.camera.heading,
+                        Cesium.Math.toRadians(-30),
+                        100
                     )
                 );
             }
         }
     });
-
 }
+
 
