f4505ee1f9f4708e18e4bad49af27815e27c596c
Boris <bhasfire@gmail.com>
2023-11-18 20:38:27 -0600

    refactoring, added speed up/down buttons

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZTVkMzhjYS1kMDc4LTRlMzYtOWNkNy05ZTY4OTZiMzNkYWIiLCJpZCI6MTc4NjIwLCJpYXQiOjE3MDAyNjA4MDJ9.exY7Ym_WUi_59ZWn18FKzw8Ue62LzQhJmdKHCd8JxUE';
 
+// import { visualizeRoute } from './scripts/routeVisualizer.js';
+
 let viewer;  // Declare the viewer at the top scope
-let isAnimating = false; // Flag to control the animation
+let animationControl = { isAnimating: false }; // Wrap isAnimating in an object
+let speedControl = { speed: 0.1 }; // Wrap speed in an object
 
 document.addEventListener('DOMContentLoaded', () => {
     viewer = new Cesium.Viewer('cesiumContainer', {
@@ -15,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         showCreditsOnScreen: false,
     }));
 
-    fetch('data/IMLP_run.gpx')
+    fetch('data/atx23_marathon.gpx')
         .then(response => response.text())
         .then(gpxText => {
             const data = parseGPX(gpxText);
@@ -27,116 +30,45 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('startButton').disabled = false;
             });
 
-            visualizeRoute(data);
+            visualizeRoute(data, viewer, animationControl, speedControl); // Pass the speed object
+
         })
         .catch(error => {
             console.error('Error fetching or parsing GPX file:', error);
         });
 
 
-    // Event listener for the start button
     document.getElementById('startButton').addEventListener('click', () => {
-        isAnimating = true;
+        animationControl.isAnimating = true;
+    });
+    
+    document.getElementById('pauseButton').addEventListener('click', () => {
+        animationControl.isAnimating = false;
     });
 
-    // Event listener for the pause button
-    document.getElementById('pauseButton').addEventListener('click', () => {
-        isAnimating = false;
+    document.getElementById('speedUpButton').addEventListener('click', () => {
+        speedControl.speed += 0.5; // Increase speed
+        console.log("Speed increased to:", speedControl.speed); // Log the new speed
     });
+    
+    document.getElementById('speedDownButton').addEventListener('click', () => {
+        speedControl.speed -= 0.5; // Decrease speed
+        if (speedControl.speed < 0.1) speedControl.speed = 0.1; // Prevent negative speed
+        console.log("Speed decreased to:", speedControl.speed); // Log the new speed
+    });
+    
 });
 
 function preloadTerrainData(coordinates, terrainProvider, callback) {
+    console.log("Preloading terrain data..."); // Log preloading start
     const samplePoints = coordinates.map(point => Cesium.Cartographic.fromDegrees(point.lon, point.lat));
-    Cesium.sampleTerrainMostDetailed(terrainProvider, samplePoints).then(callback);
-}
-
-function visualizeRoute(data) {
-    const positions = data.coordinates.map(point => 
-        Cesium.Cartesian3.fromDegrees(point.lon, point.lat, point.ele));
-    const totalPoints = positions.length;
-    let currentIndex = 0;
-    const speed = 0.1;
-
-    let currentHeading = 0;
-    let targetHeading = 0;
-
-    // Create the polyline
-    viewer.entities.add({
-        polyline: {
-            positions: positions,
-            width: 10,
-            material: new Cesium.PolylineGlowMaterialProperty({
-                color: Cesium.Color.RED.withAlpha(0.6)
-            }),
-            clampToGround: true
-        }
-    });
-
-    // Create the arrow entity
-    const routeEntity = viewer.entities.add({
-        position: positions[0],
-        model: {
-            uri: './data/Arrow_FBXImport_GLTFExport_01.gltf',
-            minimumPixelSize: 64,
-            scale: 0.1
-        },
-        label: {
-            text: data.name,
-            font: '14pt monospace',
-            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-            outlineWidth: 2,
-            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-            pixelOffset: new Cesium.Cartesian2(0, -9)
-        }
-    });
-
-    const terrainProvider = viewer.terrainProvider;
-
-    viewer.clock.onTick.addEventListener(() => {
-        if (!isAnimating) return;
-
-        currentIndex += speed;
-        if (currentIndex >= totalPoints) currentIndex = 0;
-
-        const floorIndex = Math.floor(currentIndex);
-        const nextIndex = (floorIndex + 1) % totalPoints;
-        const interpolationFactor = currentIndex - floorIndex;
-
-        const interpolatedPosition = Cesium.Cartesian3.lerp(positions[floorIndex], positions[nextIndex], interpolationFactor, new Cesium.Cartesian3());
-
-        // Calculate the heading
-        const startCartographic = Cesium.Cartographic.fromCartesian(positions[floorIndex]);
-        const endCartographic = Cesium.Cartographic.fromCartesian(positions[nextIndex]);
-        let heading = Math.atan2(endCartographic.longitude - startCartographic.longitude, endCartographic.latitude - startCartographic.latitude);
-
-        const headingOffset = Math.PI / 2; // Adjust this value as needed to align the arrow correctly
-        heading += headingOffset;
-
-        // Sample the terrain height and adjust the entity's position and orientation
-        Cesium.sampleTerrainMostDetailed(terrainProvider, [Cesium.Cartographic.fromCartesian(interpolatedPosition)])
-            .then((updatedPositions) => {
-                const updatedPosition = Cesium.Cartesian3.fromRadians(
-                    updatedPositions[0].longitude,
-                    updatedPositions[0].latitude,
-                    updatedPositions[0].height + 0.5 // Slight height above ground to avoid clipping
-                );
-                routeEntity.position = updatedPosition;
-                routeEntity.orientation = Cesium.Transforms.headingPitchRollQuaternion(updatedPosition, new Cesium.HeadingPitchRoll(heading, 0, 0));
-
-                const headingDifference = targetHeading - currentHeading;
-                currentHeading += headingDifference * 0.1; // Adjust the 0.1 factor for smoother or quicker transition
-
-                // Smooth camera movement
-                viewer.camera.lookAt(
-                    updatedPosition, 
-                    new Cesium.HeadingPitchRange(
-                        currentHeading + -Math.PI /2,
-                        Cesium.Math.toRadians(-30), // Pitch
-                        100 // Range
-                    )
-                );
-            });
+    Cesium.sampleTerrainMostDetailed(terrainProvider, samplePoints).then(() => {
+        console.log("Terrain data loaded successfully"); // Log successful loading
+        callback();
     });
 }
+
+
+
 
 
---
+++ scripts/routeVisualizer.js
@@ -0,0 +1,93 @@
+//Visualize Route Function
+
+function visualizeRoute(data, viewer, isAnimating, speed) {
+    const positions = data.coordinates.map(point => 
+        Cesium.Cartesian3.fromDegrees(point.lon, point.lat, point.ele));
+    const totalPoints = positions.length;
+    let currentIndex = 0;
+
+    let currentHeading = 0;
+    let targetHeading = 0;
+
+    // Create the polyline
+    viewer.entities.add({
+        polyline: {
+            positions: positions,
+            width: 10,
+            material: new Cesium.PolylineGlowMaterialProperty({
+                color: Cesium.Color.RED.withAlpha(0.6)
+            }),
+            clampToGround: true
+        }
+    });
+
+    // Create the arrow entity
+    const routeEntity = viewer.entities.add({
+        position: positions[0],
+        model: {
+            uri: './icons/Arrow_FBXImport_GLTFExport_01.gltf',
+            minimumPixelSize: 64,
+            scale: 0.1
+        },
+        label: {
+            text: data.name,
+            font: '14pt monospace',
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            outlineWidth: 2,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+            pixelOffset: new Cesium.Cartesian2(0, -9)
+        }
+    });
+
+    const terrainProvider = viewer.terrainProvider;
+
+    viewer.clock.onTick.addEventListener(() => {
+        if (!animationControl.isAnimating) return;
+
+        currentIndex += speedControl.speed;
+        if (currentIndex >= totalPoints) currentIndex = 0;
+
+        const floorIndex = Math.floor(currentIndex);
+        const nextIndex = (floorIndex + 1) % totalPoints;
+        const interpolationFactor = currentIndex - floorIndex;
+
+        const interpolatedPosition = Cesium.Cartesian3.lerp(positions[floorIndex], positions[nextIndex], interpolationFactor, new Cesium.Cartesian3());
+
+        // Calculate the heading
+        const startCartographic = Cesium.Cartographic.fromCartesian(positions[floorIndex]);
+        const endCartographic = Cesium.Cartographic.fromCartesian(positions[nextIndex]);
+        let heading = Math.atan2(endCartographic.longitude - startCartographic.longitude, endCartographic.latitude - startCartographic.latitude);
+
+        const headingOffset = Math.PI / 2; // Adjust this value as needed to align the arrow correctly
+        heading += headingOffset;
+
+        // Sample the terrain height and adjust the entity's position and orientation
+        Cesium.sampleTerrainMostDetailed(terrainProvider, [Cesium.Cartographic.fromCartesian(interpolatedPosition)])
+            .then((updatedPositions) => {
+                const updatedPosition = Cesium.Cartesian3.fromRadians(
+                    updatedPositions[0].longitude,
+                    updatedPositions[0].latitude,
+                    updatedPositions[0].height + 0.5 // Slight height above ground to avoid clipping
+                );
+
+                //console.log("Updated position with terrain height:", updatedPosition); // Log the updated position with terrain height
+
+                routeEntity.position = updatedPosition;
+                routeEntity.orientation = Cesium.Transforms.headingPitchRollQuaternion(updatedPosition, new Cesium.HeadingPitchRoll(heading, 0, 0));
+
+                const headingDifference = targetHeading - currentHeading;
+                currentHeading += headingDifference * 0.1; // Adjust the 0.1 factor for smoother or quicker transition
+
+                // Smooth camera movement
+                viewer.camera.lookAt(
+                    updatedPosition, 
+                    new Cesium.HeadingPitchRange(
+                        currentHeading + -Math.PI /2,
+                        Cesium.Math.toRadians(-30), // Pitch
+                        100 // Range
+                    )
+                );
+            });
+    });
+}
+window.visualizeRoute = visualizeRoute; // Attach to global scope
